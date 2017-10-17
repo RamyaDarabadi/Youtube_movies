ac04a03ae0d5634c98ff7e2f5a1d3cf86eb2e77d
Ramya Latha <ramyalatha3004@gmail.com>
2017-10-17 11:48:36 +0000

    message

--- movies.py
+++ movies.py
@@ -1,37 +1,34 @@
+"""Crawler for Youtube Movies"""
 from scrapy.spiders import CrawlSpider
 from scrapy.http import Request
 from scrapy.selector import Selector
 import MySQLdb
-
 class Movies(CrawlSpider):
+    """Nmae of Crawler"""
     name = 'movies'
     start_urls = ['https://www.youtube.com/movies']
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
+        """Connecting to Database"""
         self.conn = MySQLdb.connect(host="localhost", user="root", passwd='01491a0237db', db="Moviesdb", charset='utf8', use_unicode=True)
         self.cur = self.conn.cursor()
-
-
     def parse(self, response):
+        """Starting Nodes here"""
         sel = Selector(response)
         nodes =  sel.xpath('//ul[@id="browse-items-primary"]//li[contains(@class, "branded-page-box")]//div[@class="yt-uix-shelfslider-body"]/ul/li')
-        
         for node in nodes:
             link = ''.join(node.xpath('.//div[@class="yt-lockup-thumbnail"]/a/@href').extract())
             url = 'https://www.youtube.com'+link
             yield Request(url, callback = self.parse_movie, meta={})
-
     def parse_movie(self, response):
+        """Moving to next link"""
         sel = Selector(response)
         title = ''.join(sel.xpath('//div[@id="watch-headline-title"]/h1[@class="watch-title-container"]/span[@id="eow-title"]/text()').extract())
         publish = ''.join(sel.xpath('//div[@id="watch-uploader-info"]/strong/text()').extract())
         descr = ''.join(sel.xpath('//div[@id="watch-description-text"]/p[@id="eow-description"]/text()').extract())
         nodes = sel.xpath('//div[@id="watch-description-extras"]//li[contains(@class, "watch-meta-item ")]')
         for node in nodes:
-            
             runtime = ''.join(node.xpath('./h4[contains(text(), "Running time")]/following-sibling::ul[1]/li/text()').extract())
-            
-            qry = 'insert into movies(title, publish, descr, runtime, title)values (%s, %s, %s, %s, %s) on duplicate key update title = %s'
+            qry = 'insert into movies(title, publish, descr, runtime)values (%s, %s, %s, %s) on duplicate key update title = %s'
             values = (title, publish, descr, runtime, title)
             print qry%values
             self.cur.execute(qry, values)
